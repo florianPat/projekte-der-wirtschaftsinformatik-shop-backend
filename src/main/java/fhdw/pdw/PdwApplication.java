824a8f93841611fc9ed8dcd8ba33659e573db774
florianPat <florian.patruck@gmx.de>
2021-07-05 08:30:30 +0200

    Switch build tool from maven to gradle

--- src/main/java/fhdw/pdw/PdwApplication.java
+++ src/main/java/fhdw/pdw/PdwApplication.java
@@ -4,13 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableSwagger2
 @EntityScan(basePackageClasses = {
-		PdwApplication.class,
+		fhdw.pdw.PdwApplication.class,
 		Jsr310JpaConverters.class
 })
 public class PdwApplication {
@@ -19,6 +21,6 @@ public class PdwApplication {
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 	}
 	public static void main(String[] args) {
-		SpringApplication.run(PdwApplication.class, args);
+		SpringApplication.run(fhdw.pdw.PdwApplication.class, args);
 	}
 }
