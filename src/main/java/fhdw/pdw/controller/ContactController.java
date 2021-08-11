a89c73fa369ccef0515b91ee32991c0b3a8972f3
florianPat <florian.patruck@gmx.de>
2021-08-11 10:13:01 +0200

    Add contact form endpoint which sends an email

---
+++ src/main/java/fhdw/pdw/controller/ContactController.java
@@ -0,0 +1,35 @@
+package fhdw.pdw.controller;
+
+import fhdw.pdw.email.EmailService;
+import fhdw.pdw.model.dto.ApiResponse;
+import fhdw.pdw.model.dto.ContactFormMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RequestMapping("/api")
+@RestController
+public class ContactController {
+    protected EmailService emailService;
+
+    public ContactController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @PostMapping("/contact")
+    public ResponseEntity<?> postContactForm(@Valid @RequestBody ContactFormMessage contactFormMessage) {
+        emailService.sendSimpleMessage(
+                "siphydrated@gmail.com",
+                "Kontaktformular: " + contactFormMessage.getSubject(),
+                "Von: " + contactFormMessage.getName() + ", " + contactFormMessage.getEmail() + "\n" +
+                        contactFormMessage.getText()
+        );
+
+        return new ResponseEntity<>(new ApiResponse(true, "Message send successfully"), HttpStatus.OK);
+    }
+}
