771959aab180754324ef1fe332e849a3ceb2028e
florianPat <florian.patruck@gmx.de>
2021-08-15 22:10:05 +0200

    Fix migration by adding default value to alter column statement

--- src/main/resources/db/migration/V5_1__order_age_verfiy.sql
+++ src/main/resources/db/migration/V5_1__order_age_verfiy.sql
@@ -1,3 +1,3 @@
 alter table orders add column created_at timestamp;
-alter table product add column min_age int4 not null;
-alter table users add column has_verified_age boolean not null;
+alter table product add column min_age int4 not null default 0;
+alter table users add column has_verified_age boolean not null default false;
