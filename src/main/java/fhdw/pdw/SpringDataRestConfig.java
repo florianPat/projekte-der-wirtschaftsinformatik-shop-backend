a4ac3cd67e96e6ae10a00930ba571266ba0bb453
florianPat <florian.patruck@gmx.de>
2021-08-28 12:57:58 +0200

    Add validation to the data rest endpoints

--- src/main/java/fhdw/pdw/SpringDataRestConfig.java
+++ src/main/java/fhdw/pdw/SpringDataRestConfig.java
@@ -4,13 +4,16 @@ import javax.persistence.EntityManager;
 import javax.persistence.metamodel.Type;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.Validator;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Component
 public class SpringDataRestConfig implements RepositoryRestConfigurer {
-  @Autowired private EntityManager entityManager;
+  @Autowired protected EntityManager entityManager;
+  @Autowired protected Validator validator;
 
   @Override
   public void configureRepositoryRestConfiguration(
@@ -24,5 +27,16 @@ public class SpringDataRestConfig implements RepositoryRestConfigurer {
         entityManager.getMetamodel().getEntities().stream()
             .map(Type::getJavaType)
             .toArray(Class[]::new));
+  }
+
+  @Override
+  public void configureValidatingRepositoryEventListener(
+      ValidatingRepositoryEventListener validatingListener) {
+    validatingListener.addValidator("afterCreate", validator);
+    validatingListener.addValidator("beforeCreate", validator);
+    validatingListener.addValidator("afterSave", validator);
+    validatingListener.addValidator("beforeSave", validator);
+    validatingListener.addValidator("beforeLinkSave", validator);
+    validatingListener.addValidator("afterLinkSave", validator);
   }
 }
